a1dc5a9315684d6b33d0ba3c4add8c8643e80a96
Nobuyuki Kimura <a09@r-force.co.jp>
2022-10-26 16:05:49 +0900

    最初変更

--- src/jp/co/amgakuin/javaclss2022second/sample/Sample.java
+++ src/jp/co/amgakuin/javaclss2022second/sample/Sample.java
@@ -2,6 +2,12 @@ package jp.co.amgakuin.javaclss2022second.sample;
 
 public class Sample {
 
+	//クラスのフィールド
+	public int fieldA;
+	
+	//定数は？
+	public final static int CONST_NUMBER=10;
+	
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello World!!");
