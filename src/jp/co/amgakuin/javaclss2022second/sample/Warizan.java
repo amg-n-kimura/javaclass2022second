3f6006db1d6187b82c4dce0775af03b8b665d103
石塚唯純 <122pg0024@tokyo.amg.ac.jp>
2022-10-26 17:09:25 +0900

    割り算

--- src/jp/co/amgakuin/javaclss2022second/sample/Warizan.java
+++ src/jp/co/amgakuin/javaclss2022second/sample/Warizan.java
@@ -1,10 +1,20 @@
 package jp.co.amgakuin.javaclss2022second.sample;
 
 public class Warizan {
-
+	public int waru(int a, int b) {
+		if(a==0||b==0) {
+			return 0;
+		}
+		else 
+		{
+		 return a/b;
+	    }
+	}
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		
+			Warizan w = new Warizan();
+			int kotae = w.waru(20, 10);
+			System.out.println(kotae);
 	}
 
 }
