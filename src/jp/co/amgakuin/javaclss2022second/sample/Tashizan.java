d6a3b1094a8b01f9cb51e0012e27ebeac3b307ba
TsuchiyaSuzuno <122pg0023@tokyo.amg.ac.jp>
2022-10-26 16:26:32 +0900

    足し算クラス作成

---
+++ src/jp/co/amgakuin/javaclss2022second/sample/Tashizan.java
@@ -0,0 +1,16 @@
+package jp.co.amgakuin.javaclss2022second.sample;
+
+public class Tashizan {
+
+	public int Tasu(int a, int b) {
+		return a+b;
+	}
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Tashizan k = new Tashizan();
+		int kotae = k.Tasu(30, 45);
+		System.out.println(kotae);
+	}
+
+}
