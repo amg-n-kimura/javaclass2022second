207589fd4b4dd07529bbf4690e3879e0ff7ade68
響 雷電 <raidennhibiki@yahoo.co.jp>
2022-10-26 16:25:37 +0900

    WH

--- src/jp/co/amgakuin/javaclss2022second/sample/Sample.java
+++ src/jp/co/amgakuin/javaclss2022second/sample/Sample.java
@@ -1,10 +1,14 @@
 package jp.co.amgakuin.javaclss2022second.sample;
 
+/**
+ * @author amg
+ *
+ */
 public class Sample {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello World!!");
+		System.out.println("HelloHibiki!!");
 	}
 
 }
