7071b1d727e760c26174c9bd635777361e7a95cc
122pg0001 <122pg0001@tokyo.amg.ac.jp>
2022-10-26 16:27:54 +0900

    足し算

---
+++ src/jp/co/amgakuin/javaclss2022second/sample/Tashizan.java
@@ -0,0 +1,17 @@
+package jp.co.amgakuin.javaclss2022second.sample;
+
+public class Tashizan {
+
+	public int tasu(int a, int b) {
+		return a+b;
+	}
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Tashizan k = new Tashizan();
+		int kotae = k.tasu(20, 10);
+		System.out.println(kotae);
+	}
+
+}
