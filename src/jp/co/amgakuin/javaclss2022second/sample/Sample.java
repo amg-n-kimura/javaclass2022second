aa039fac5c39729d69d2bed7356005c5b8496ad9
宮本孟守人 <122pg0006@tokyo.amg.ac.jp>
2022-10-26 16:05:52 +0900

    最初の変更

--- src/jp/co/amgakuin/javaclss2022second/sample/Sample.java
+++ src/jp/co/amgakuin/javaclss2022second/sample/Sample.java
@@ -1,10 +1,16 @@
 package jp.co.amgakuin.javaclss2022second.sample;
 
 public class Sample {
+	//クラスのフィールド
+		public int fieldA;
+		
+		//定数は？
+		public final static int CONST_NUMBER=10;
+
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello World!!");
+		System.out.println("Hello Miyamoto!!");
 	}
 
 }
