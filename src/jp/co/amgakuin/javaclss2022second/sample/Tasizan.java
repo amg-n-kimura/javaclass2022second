9881bb1df9917745b9de0153d34ec856c542d98a
阿部健太郎 <122pg0009@tokyo.amg.ac.jp>
2022-10-26 16:31:04 +0900

    足し算クラス作成

---
+++ src/jp/co/amgakuin/javaclss2022second/sample/Tasizan.java
@@ -0,0 +1,16 @@
+package jp.co.amgakuin.javaclss2022second.sample;
+
+public class Tasizan {
+	
+	public int tasu(int a, int b) {
+		return a+b;
+	}
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Tasizan t = new Tasizan();
+		int kotae = t.tasu(20, 10);
+		System.out.println(kotae);
+	}
+
+}
