f36d966bafe41652ea09f7375226ef0450b79597
渡邉彩香 <122pg0002@tokyo.amg.ac.jp>
2022-10-26 16:31:52 +0900

    足し算

---
+++ src/jp/co/amgakuin/javaclss2022second/sample/Tashizan.java
@@ -0,0 +1,21 @@
+package jp.co.amgakuin.javaclss2022second.sample;
+
+public class Tashizan {
+	
+	
+	public int Tasu(int a,int b) {
+		return a+b;
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+	
+		
+		Tashizan t=new Tashizan();
+		System.out.println(t.Tasu(10,8));
+		
+		
+
+	}
+
+}
