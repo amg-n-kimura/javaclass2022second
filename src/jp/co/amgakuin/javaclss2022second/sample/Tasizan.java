f89f6771b969212d4f5aeec08b478754ac0c000b
中田瑞希 <122pg0013@tokyo.amg.ac.jp>
2022-10-26 16:29:35 +0900

    足し算追加

---
+++ src/jp/co/amgakuin/javaclss2022second/sample/Tasizan.java
@@ -0,0 +1,17 @@
+package jp.co.amgakuin.javaclss2022second.sample;
+
+public class Tasizan {
+	
+	public int tasu(int a, int b) {
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Tasizan t = new Tasizan();
+		int Kotae = t.tasu(5, 5);
+		System.out.println(Kotae);
+	}
+
+}
