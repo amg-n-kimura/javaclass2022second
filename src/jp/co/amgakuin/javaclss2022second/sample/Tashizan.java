7c50db3521febc2a8fd8dc0b6abd4ce7d6f633d9
Nobuyuki Kimura <a09@r-force.co.jp>
2022-10-26 16:59:16 +0900

    Tashizanクラス追加

---
+++ src/jp/co/amgakuin/javaclss2022second/sample/Tashizan.java
@@ -0,0 +1,16 @@
+package jp.co.amgakuin.javaclss2022second.sample;
+
+public class Tashizan {
+
+	public int tasu(int a, int b) {
+		return (a+b);
+	}
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Tashizan t = new Tashizan();
+		System.out.println(t.tasu(10, 8));
+	}
+
+}
