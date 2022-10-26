15764b1b2cdd0dca9c18e6f9170831b88dae6daa
Sakai Hiroaki <122pg0008@tokyo.amg.ac.jp>
2022-10-26 16:33:52 +0900

    足し算クラス追加

---
+++ src/jp/co/amgakuin/javaclss2022second/sample/tashizan.java
@@ -0,0 +1,16 @@
+package jp.co.amgakuin.javaclss2022second.sample;
+
+public class tashizan {
+
+	public int tasu(int a, int b) {
+		return a + b;
+	}
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        tashizan k = new tashizan();
+        int kotae = k.tasu(20, 10);
+        System.out.println(kotae);
+		
+	}
+
+}
