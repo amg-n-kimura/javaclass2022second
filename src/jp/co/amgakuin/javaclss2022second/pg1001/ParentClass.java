9c83b217d0ac132a8e1b875269ecd4b46d955551
脇添　敏暉 <122pg1001@tokyo.amg.ac.jp>
2022-11-09 16:37:16 +0900

    sisoku

---
+++ src/jp/co/amgakuin/javaclss2022second/pg1001/ParentClass.java
@@ -0,0 +1,10 @@
+package jp.co.amgakuin.javaclss2022second.pg1001;
+
+public class ParentClass {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ src/jp/co/amgakuin/javaclss2022second/pg1001/Warizan.java
@@ -0,0 +1,21 @@
+
+package jp.co.amgakuin.javaclss2022second.pg1001;
+
+public class Warizan {
+
+	public int waru(int a,int b) {
+		if(a  == 0 || b == 0) {
+			
+		}
+		return a/b;
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		 Warizan t = new Warizan();
+         int kotae = t.waru(18,6);
+         System.out.println(kotae);
+	}
+
+}
