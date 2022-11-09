d3e3eea97e2753db4ba4e58567fbd8f313ecf336
石塚唯純 <122pg0024@tokyo.amg.ac.jp>
2022-11-09 16:36:50 +0900

    Parent

---
+++ src/jp/co/amgakuin/javaclss2022second/pg0024/c20221109/ParentClass.java
@@ -0,0 +1,10 @@
+package jp.co.amgakuin.javaclss2022second.pg0024.c20221109;
+
+public class ParentClass {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ src/jp/co/amgakuin/javaclss2022second/pg0024/c20221109/TestClass.java
@@ -0,0 +1,10 @@
+package jp.co.amgakuin.javaclss2022second.pg0024.c20221109;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
