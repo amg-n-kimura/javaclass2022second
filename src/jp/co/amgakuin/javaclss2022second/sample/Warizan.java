955bf8fce096ce184b275f5c8bac57e2851dc883
脇添　敏暉 <122pg1001@tokyo.amg.ac.jp>
2022-11-09 15:42:54 +0900

    割り算

--- src/jp/co/amgakuin/javaclss2022second/sample/Warizan.java
+++ src/jp/co/amgakuin/javaclss2022second/sample/Warizan.java
@@ -1,5 +1,21 @@
+
 package jp.co.amgakuin.javaclss2022second.sample;
 
 public class Warizan {
+
+	public int waru(int a,int b) {
+		if(a  == 0 || b == 0) {
+			
+		}
+		return a/b;
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		 Warizan t = new Warizan();
+         int kotae = t.waru(18,6);
+         System.out.println(kotae);
+	}
 
 }
