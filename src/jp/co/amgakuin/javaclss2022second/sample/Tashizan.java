e4de39a9c203847287d12d644f6c1495ee11d4dd
Nobuyuki Kimura <a09@r-force.co.jp>
2022-11-09 14:13:01 +0900

    足し算実装

--- src/jp/co/amgakuin/javaclss2022second/sample/Tashizan.java
+++ src/jp/co/amgakuin/javaclss2022second/sample/Tashizan.java
@@ -9,6 +9,13 @@ public class Tashizan {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		
+		Integer i1;
+		int i2;
+		
+		i2 = 10;
+		i1 = 10;
+		
 		Tashizan t = new Tashizan();
 		System.out.println(t.tasu(10, 8));
 	}
