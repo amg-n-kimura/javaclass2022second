8f044bf810f7fd03aaf60b262c5d6d156555af76
松田裕 <122pg0017@tokyo.amg.ac.jp>
2022-11-09 16:31:09 +0900

    割り算実装、足し算修正

--- src/jp/co/amgakuin/javaclss2022second/sampleresult/Warizan.java
+++ src/jp/co/amgakuin/javaclss2022second/sampleresult/Warizan.java
@@ -8,26 +8,7 @@ public class Warizan {
 	public Warizan(){	
 	}
 	
-	public int warizan(int a,int b) {
-		int result;
-		//例外処理
-		try {
-			result=a/b;
-		}catch(Exception e) {
-			result=0;
-		}
-		
-		return a/b;
-	}
-	
-	public static void main(String[]args) {
-		// TOD0 自動生成されたメソッド・スタブ
-		Warizan warizan = new Warizan();
-		int a =10;
-		int b=0;
-		int result =warizan.warizan(a, b);
-		System.out.println(a+"/"+b+"="+ result);
-	}
+
 	public int warizan(int a, int b) {
 		int result;
 		//例外処理
