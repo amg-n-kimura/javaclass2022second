1af6efc047a8a722d1ddc4a49f9973b7a9b54f7c
石塚唯純 <122pg0024@tokyo.amg.ac.jp>
2022-11-30 16:26:38 +0900

    frameworkとshikaku

---
+++ src/jp/co/amgakuin/javaclss2022second/samplef/Shikaku.java
@@ -0,0 +1,32 @@
+
+package jp.co.amgakuin.javaclss2022second.samplef;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import jp.co.amgakuin.javaclss2022second.framework.DisplayObject;
+
+public class Shikaku extends DisplayObject {
+
+	@Override
+	public boolean update() {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		// TODO 自動生成されたメソッド・スタブ
+		g.setColor(Color.GREEN);
+		g.fillRect(0, 0, 100, 100);
+	}
+
+	@Override
+	public void initialize() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
+
+
