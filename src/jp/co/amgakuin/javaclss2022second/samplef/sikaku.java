2004759beccf051f79118db926ac0d987027ebab
riku yamamoto <raidennhibiki@yahoo.co.jp>
2022-11-30 17:09:37 +0900

    荒ぶる■とランダム配置な■x9

--- src/jp/co/amgakuin/javaclss2022second/samplef/sikaku.java
+++ src/jp/co/amgakuin/javaclss2022second/samplef/sikaku.java
@@ -2,27 +2,52 @@ package jp.co.amgakuin.javaclss2022second.samplef;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 import jp.co.amgakuin.javaclss2022second.framework.DisplayObject;
 
 public class sikaku extends DisplayObject {
-
+	int posX[] = new int[10], posY[] = new int[10], waitTime, start = 0;
+	Random random = new Random();
+	
 	@Override
 	public boolean update() {
 		// TODO 自動生成されたメソッド・スタブ
+		if(waitTime == 0)
+		{
+			posX[0] = random.nextInt(540);
+			posY[0] = random.nextInt(380);
+			
+			waitTime = 7;
+		}
+		else {waitTime--;}
+		
 		return false;
 	}
 
 	@Override
 	public void draw(Graphics g) {
 		// TODO 自動生成されたメソッド・スタブ
+		
 		g.setColor(Color.GREEN);
-		g.fillRect(0, 0, 100, 100);
+		for(int i = 0; i < 10; i++)
+		{
+			g.fillRect(posX[i], posY[i], 100, 100);
+		}
 	}
 
 	@Override
 	public void initialize() {
 		// TODO 自動生成されたメソッド・スタブ
+		if(start == 0)
+		{
+			for(int i = 0; i < 10; i++)
+			{
+				posX[i] = random.nextInt(540);
+				posY[i] = random.nextInt(380);
+			}
+			start++;
+		}
 
 	}
 
