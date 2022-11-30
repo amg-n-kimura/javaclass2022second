2fcd71770bd8b0c7498de8558b1473e7541cf388
DoiShotaro <122pg0004@tokyo.amg.ac.jp>
2022-11-30 17:06:51 +0900

    お題提出

--- src/jp/co/amgakuin/javaclss2022second/samplef/Shikaku.java
+++ src/jp/co/amgakuin/javaclss2022second/samplef/Shikaku.java
@@ -2,14 +2,33 @@ package jp.co.amgakuin.javaclss2022second.samplef;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 import jp.co.amgakuin.javaclss2022second.framework.DisplayObject;
+import lombok.Setter;
 
 public class Shikaku extends DisplayObject {
-
+    @Setter
+	private boolean moveFlag=false;
+    private boolean left =false;
 	@Override
 	public boolean update() {
 		// TODO 自動生成されたメソッド・スタブ
+		if(!moveFlag) {
+			return false;
+		}
+		if(left) {
+			setX(getX()-1);
+		}else {
+			setX(getX()+1);
+		}
+		if(getX()>640) {
+			left =true;
+		}
+		if(getX()<0) {
+			left =false;
+		}
+		
 		return false;
 	}
 
@@ -17,13 +36,16 @@ public class Shikaku extends DisplayObject {
 	public void draw(Graphics g) {
 		// TODO 自動生成されたメソッド・スタブ
 		g.setColor(Color.GREEN);
-		g.fillRect(0, 0, 100, 100);
+		g.fillRect(getX(), getY(),100 , 100);
 	}
-
+	
 	@Override
 	public void initialize() {
 		// TODO 自動生成されたメソッド・スタブ
-
+		Random random =new Random();
+		setX(random.nextInt(640));
+		setY(random.nextInt(480));
+		initialized =true;
 	}
 
 }
--- src/jp/co/amgakuin/javaclss2022second/samplef/frameworkSample.java
+++ src/jp/co/amgakuin/javaclss2022second/samplef/frameworkSample.java
@@ -8,7 +8,12 @@ public class frameworkSample {
 		// TODO 自動生成されたメソッド・スタブ
 		GameController gc=GameController.gameControllerCreate(640,480);
 		gc.start();
-		gc.addObject(new Shikaku());
+		for(int i=0;i<10;++i) {
+			gc.addObject(new Shikaku());
+		}
+		var moveShikaku=new Shikaku();
+		moveShikaku.setMoveFlag(true);
+		gc.addObject(moveShikaku);
 	}
 
 }
