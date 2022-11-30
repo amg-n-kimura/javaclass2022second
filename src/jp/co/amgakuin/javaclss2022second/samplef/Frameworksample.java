0005aee4c2f2413822f0241801a7fea106a0a2b8
Sakai Hiroaki <122pg0008@tokyo.amg.ac.jp>
2022-11-30 17:13:49 +0900

    20221130

---
+++ src/jp/co/amgakuin/javaclss2022second/samplef/Frameworksample.java
@@ -0,0 +1,16 @@
+package jp.co.amgakuin.javaclss2022second.samplef;
+
+import jp.co.amgakuin.javaclss2022second.framework.GameController;
+
+
+public class Frameworksample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+			// TODO 自動生成されたメソッド・スタブ
+			GameController gc = GameController.gameControllerCreate(640, 480);
+			gc.start();
+
+	}
+
+}
