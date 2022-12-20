707cae3c10ef0db19b6a36c07bd63ce9a834867c
takeru isojima <122pg0020@tokyo.amg.ac.jp>
2022-12-20 16:11:16 +0900

    四角課題1,2複合(横移動)

--- src/jp/co/amgakuin/javaclss2022second/sanplef/Shikaku.java
+++ src/jp/co/amgakuin/javaclss2022second/sanplef/Shikaku.java
@@ -2,27 +2,53 @@ package jp.co.amgakuin.javaclss2022second.sanplef;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 import jp.co.amgakuin.javaclss2022second.framework.DisplayObject;
 
 public class Shikaku extends DisplayObject {
-
+	int posX[] = new int[10], posY[] = new int[10], waitTime, start = 0;
+	Random random = new Random();
+	int move=0;
+	
 	@Override
 	public boolean update() {
 		// TODO 自動生成されたメソッド・スタブ
+		if(posX[0]>=540) {move=1;}
+		if(posX[0]<=0) {move=0;}
+		if(move==0) {
+			posX[0] += 1;
+		}
+		if(move==1) {
+			posX[0] -= 1;
+		}
+		
 		return false;
 	}
 
 	@Override
 	public void draw(Graphics g) {
 		// TODO 自動生成されたメソッド・スタブ
+		
 		g.setColor(Color.GREEN);
-		g.fillRect(0, 0, 100, 100);
+		for(int i = 0; i < 10; i++)
+		{
+			g.fillRect(posX[i], posY[i], 100, 100);
+		}
 	}
 
 	@Override
 	public void initialize() {
 		// TODO 自動生成されたメソッド・スタブ
+		if(start == 0)
+		{
+			for(int i = 0; i < 10; i++)
+			{
+				posX[i] = random.nextInt(540);
+				posY[i] = random.nextInt(380);
+			}
+			start++;
+		}
 
 	}
 
