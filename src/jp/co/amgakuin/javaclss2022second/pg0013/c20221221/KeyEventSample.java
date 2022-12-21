e56be48c3071bb8b33dbd30d195f9cbb7a390615
中田瑞希 <122pg0013@tokyo.amg.ac.jp>
2022-12-21 17:26:54 +0900

    四角移動

---
+++ src/jp/co/amgakuin/javaclss2022second/pg0013/c20221221/KeyEventSample.java
@@ -0,0 +1,57 @@
+package jp.co.amgakuin.javaclss2022second.pg0013.c20221221;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import jp.co.amgakuin.javaclss2022second.framework.GameController;
+import lombok.Setter;
+
+public class KeyEventSample implements KeyListener {
+	@Setter
+	static private Shikaku shikaku;
+	
+	public static void main(String[] args) {
+		KeyEventSample key = new KeyEventSample();
+		shikaku = new Shikaku();
+		KeyEventSample.setShikaku(shikaku);
+		GameController gc = GameController.gameControllerCreate(640, 480);
+		gc.addKeyListener(key);
+		gc.addObject(shikaku);
+		gc.start();
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x = shikaku.getX();
+		int y = shikaku.getY();
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			x += 10;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			x -= 10;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+			y += 10;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_UP) {
+			y -= 10;
+		}
+		
+		shikaku.setX(x);
+		shikaku.setY(y);
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+}
