cd4adb18fba3eaaaa515e70105f4ae0df2ca154a
inoue <122pg@tokyo.amg.ac.jp>
2022-12-21 15:42:38 +0900

    四角の配列

--- src/jp/co/amgakuin/javaclss2022second/samplef/FrameworkSample.java
+++ src/jp/co/amgakuin/javaclss2022second/samplef/FrameworkSample.java
@@ -7,7 +7,6 @@ public class FrameworkSample {
 		// TODO 自動生成されたメソッド・スタブ
 		GameController gc = GameController.gameControllerCreate(640, 480);
 		gc.start();
-		
 		for(int i=0;i<11;++i) {
 		gc.addObject(new Shikaku());
 		}
