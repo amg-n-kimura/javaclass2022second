e3c7b9f018bf80ed30af7b6f23c76c3294896a93
Sakai Hiroaki <122pg0008@tokyo.amg.ac.jp>
2022-12-21 17:24:09 +0900

    四角移動

--- src/jp/co/amgakuin/javaclss2022second/sample20221221key/KeyEventSample.java
+++ src/jp/co/amgakuin/javaclss2022second/sample20221221key/KeyEventSample.java
@@ -8,14 +8,17 @@ import lombok.Setter;
 
 public class KeyEventSample implements KeyListener {
 	@Setter
-	private Shikaku shikaku;
+	private static Shikaku shikaku;
 	
+	@SuppressWarnings("static-access")
 	public static void main(String[] args) {
 		KeyEventSample key = new KeyEventSample();
 		key.setShikaku(new Shikaku());
 		GameController gc = GameController.gameControllerCreate(640, 480);
 		gc.addKeyListener(key);
+		gc.addObject(shikaku);
 		gc.start();
+		
 	}
 	
 	@Override
@@ -26,8 +29,34 @@ public class KeyEventSample implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
+		int oldx = shikaku.getX();
+		int oldy = shikaku.getY();
 		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
-			System.out.println("右");
+			int X = shikaku.getX();
+			X += 10;
+			shikaku.setX(X);
+		}
+		if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			int X = shikaku.getX();
+			X -= 10;
+			shikaku.setX(X);
+		}
+		if(e.getKeyCode() == KeyEvent.VK_UP) {
+			int Y = shikaku.getY();
+			Y -= 10;
+			shikaku.setY(Y);
+		}
+		if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+			int Y = shikaku.getY();
+			Y += 10;
+			shikaku.setY(Y);
+		}
+		int x, y;
+		x = shikaku.getX();
+		y = shikaku.getY();
+		if(x < 0 || x > 540 || y < 0 || y > 380) {
+			shikaku.setX(oldx);
+			shikaku.setY(oldy);
 		}
 		
 	}
