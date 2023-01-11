b78cdab3659923c2df082fe627eae8a380a7b681
takeru isojima <122pg0020@tokyo.amg.ac.jp>
2023-01-11 13:41:04 +0900

    unknown

--- src/jp/co/amgakuin/javaclss2022second/sample20221221key/KeyEventSample.java
+++ src/jp/co/amgakuin/javaclss2022second/sample20221221key/KeyEventSample.java
@@ -9,6 +9,7 @@ import lombok.Setter;
 public class KeyEventSample implements KeyListener {
 	@Setter
 	private Shikaku shikaku;
+	int x,y;
 	
 	public static void main(String[] args) {
 		KeyEventSample key = new KeyEventSample();
@@ -26,8 +27,20 @@ public class KeyEventSample implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
+		x = shikaku.getX();
+		y = shikaku.getY();
 		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			//shikaku.setX(++x);
 			System.out.println("右");
+		}
+		if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			shikaku.setX(--x);
+		}
+		if(e.getKeyCode() == KeyEvent.VK_UP) {
+			shikaku.setY(++y);
+		}
+		if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+			shikaku.setY(--y);
 		}
 		
 	}
