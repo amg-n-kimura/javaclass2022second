36e84fb024f65f1df13b4b90f636e00712780017
122pg0001 <122pg0001@tokyo.amg.ac.jp>
2023-02-01 15:06:51 +0900

    マウスサンプルをコピー

---
+++ src/jp/co/amgakuin/javaclss2022second/pg0001/c20230201/MouseSampleMain.java
@@ -0,0 +1,81 @@
+package jp.co.amgakuin.javaclss2022second.pg0001.c20230201;
+
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.Random;
+
+import jp.co.amgakuin.javaclss2022second.framework.GameController;
+import jp.co.amgakuin.javaclss2022second.framework.GameControllerObject;
+
+/**
+ * マウスで四角を描画する
+ * @author rf-nobuyuki-kimura
+ *
+ *必須事項
+ *	MouseListenerの実装
+ *	描画する四角（描画オブジェクト）を管理する
+ */
+public class MouseSampleMain implements MouseListener {
+	
+	//乱数用インスタンス
+	Random random = new Random();
+	
+	//描画するオブジェクトの管理用配列
+	private ArrayList<GameControllerObject> myObjects = new ArrayList<GameControllerObject>();
+	
+	//ゲームコントローラーインスタンス
+	private GameController gc = null;
+	
+	public void startSample() {
+		gc = GameController.gameControllerCreate(640, 480);
+		gc.addMouseListener(this);
+		gc.start();
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		MouseSampleMain m = new MouseSampleMain();
+		m.startSample();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		int mouseX = gc.normalizeX(e.getX());
+		int mouseY = gc.normalizeY(e.getY());
+		Shikaku s = new Shikaku();
+		s.setX(mouseX);
+		s.setY(mouseY);
+		s.setWidth(20);
+		s.setHeight(20);
+		s.setColor(new Color(random.nextInt(256),random.nextInt(256),random.nextInt(256),255));
+		gc.addObject(s);
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+}
