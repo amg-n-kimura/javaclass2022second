a3e60d5e184ff0cb83377faac6448fba8cf17224
渡邉彩香 <122pg0002@tokyo.amg.ac.jp>
2023-02-01 16:27:02 +0900

    変更

--- src/Watanabe20230201/MouseSampleMain.java
+++ src/Watanabe20230201/MouseSampleMain.java
@@ -8,6 +8,7 @@ import java.util.Random;
 
 import jp.co.amgakuin.javaclss2022second.framework.GameController;
 import jp.co.amgakuin.javaclss2022second.framework.GameControllerObject;
+import jp.co.amgakuin.javaclss2022second.sample20230201.Shikaku;
 
 /**
  * マウスで四角を描画する
@@ -43,17 +44,25 @@ public class MouseSampleMain implements MouseListener {
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
-		if(e.getButton()==MouseEvent.BUTTON1) {
-		int mouseX = gc.normalizeX(e.getX());
-		int mouseY = gc.normalizeY(e.getY());
-		Shikaku s = new Shikaku();
-		s.setX(mouseX);
-		s.setY(mouseY);
-		s.setWidth(20);
-		s.setHeight(20);
-		s.setColor(new Color(random.nextInt(256),random.nextInt(256),random.nextInt(256),255));
-		gc.addObject(s);
-		gc.addObject(s);
+		//左ボタンでオブジェクト追加
+		if (e.getButton() == MouseEvent.BUTTON1) {
+			int mouseX = gc.normalizeX(e.getX());
+			int mouseY = gc.normalizeY(e.getY());
+			Shikaku s = new Shikaku();
+			s.setX(mouseX);
+			s.setY(mouseY);
+			s.setWidth(20);
+			s.setHeight(20);
+			s.setColor(new Color(random.nextInt(256),random.nextInt(256),random.nextInt(256),255));
+			gc.addObject(s);
+			myObjects.add(s);
+			System.out.println("オブジェクト追加");
+		} else if (e.getButton() == MouseEvent.BUTTON3) {
+			//左ボタンで古いオブジェクトを削除
+			if (!myObjects.isEmpty()) {
+				gc.removeObject(myObjects.remove(0));
+				System.out.println("オブジェクト削除");
+			}
 		}
 	
 	}
