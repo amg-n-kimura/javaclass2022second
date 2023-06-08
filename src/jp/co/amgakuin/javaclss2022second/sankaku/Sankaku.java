f08051711c08324defecfea3177a34f44678b16f
122pg0020 <122pg0020@tokyo.amg.ac.jp>
2023-06-08 13:17:23 +0900

    三角形描画

---
+++ src/jp/co/amgakuin/javaclss2022second/sankaku/Sankaku.java
@@ -0,0 +1,36 @@
+package jp.co.amgakuin.javaclss2022second.sankaku;
+
+import java.awt.Graphics;
+
+import jp.co.amgakuin.javaclss2022second.framework.DisplayObject;
+
+/**
+ * 四角を描画するクラス
+ * @author rf-nobuyuki-kimura
+ *
+ */
+public class Sankaku extends DisplayObject {
+
+	@Override
+	public boolean update() {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		// TODO 自動生成されたメソッド・スタブ
+		g.setColor(getColor());
+		//g.fillRect(getX(), getY(), getWidth(), getHeight());
+		g.drawLine(getX(), getY(), getX() + getWidth(), getY());
+		g.drawLine(getX(), getY(), getX(), getY() + getHeight());
+		g.drawLine(getX(), getY() + getHeight(), getX() + getWidth(), getY());
+	}
+
+	@Override
+	public void initialize() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
