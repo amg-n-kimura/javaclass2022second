b6a024f4faa14bf46dbc32e4e5f04e3f5875dca2
takeru isojima <122pg0020@tokyo.amg.ac.jp>
2023-06-23 16:01:12 +0900

    左ボタンで円、右ボタンで線を描画、ホイールボタンで古いオブジェクトの削除

---
+++ src/jp/co/amgakuin/javaclss2022second/sankaku/Line.java
@@ -0,0 +1,29 @@
+package jp.co.amgakuin.javaclss2022second.sankaku;
+
+import java.awt.Graphics;
+
+import jp.co.amgakuin.javaclss2022second.framework.DisplayObject;
+
+public class Line extends DisplayObject {
+
+	@Override
+	public boolean update() {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		// TODO 自動生成されたメソッド・スタブ
+		g.setColor(getColor());
+		g.drawLine(getX(), getY(), getX()+getWidth(), getY()+getHeight());
+
+	}
+
+	@Override
+	public void initialize() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
--- src/jp/co/amgakuin/javaclss2022second/sankaku/MouseSampleMain.java
+++ src/jp/co/amgakuin/javaclss2022second/sankaku/MouseSampleMain.java
@@ -52,15 +52,28 @@ public class MouseSampleMain implements MouseListener {
 		if (e.getButton() == MouseEvent.BUTTON1) {
 			int mouseX = gc.normalizeX(e.getX());
 			int mouseY = gc.normalizeY(e.getY());
-			Sankaku s = new Sankaku();
-			s.setX(mouseX);
-			s.setY(mouseY);
-			s.setWidth(20);
-			s.setHeight(20);
-			s.setColor(new Color(random.nextInt(256),random.nextInt(256),random.nextInt(256),255));
-			gc.addObject(s);
-			myObjects.add(s);
-			System.out.println("オブジェクト追加");
+			Oval o=new Oval();
+			o.setX(mouseX);
+			o.setY(mouseY);
+			o.setWidth(20);
+			o.setHeight(20);
+			o.setColor(new Color(random.nextInt(256),random.nextInt(256),random.nextInt(256),255));
+			gc.addObject(o);
+			myObjects.add(o);
+			System.out.println("〇追加");
+			
+		} else if (e.getButton() == MouseEvent.BUTTON2) {
+			int mouseX = gc.normalizeX(e.getX());
+			int mouseY = gc.normalizeY(e.getY());
+			Line l = new Line();
+			l.setX(mouseX);
+			l.setY(mouseY);
+			l.setWidth(20);
+			l.setHeight(20);
+			l.setColor(new Color(random.nextInt(256),random.nextInt(256),random.nextInt(256),255));
+			gc.addObject(l);
+			myObjects.add(l);
+			System.out.println("線追加");
 		} else if (e.getButton() == MouseEvent.BUTTON3) {
 			//左ボタンで古いオブジェクトを削除
 			if (!myObjects.isEmpty()) {
